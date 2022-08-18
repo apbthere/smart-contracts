4ab47d9642f2a0aa81a69dbef555e7a673684d25
apbthere <abcconsultingit@gmail.com>
2022-08-18 09:26:13 -0400

    switch to hardhat-chai-matchers

--- test/votertoken.test.ts
+++ test/votertoken.test.ts
@@ -32,14 +32,14 @@ describe("VoterToken", function () {
   describe("transferFrom", function () {
     it("transferFrom is not supported", async function () {
       await expect(votertoken.transferFrom(await acc1.getAddress(), await acc2.getAddress(), 1)).to.be
-        .revertedWith('NotSupported()');
+        .revertedWithCustomError(votertoken, 'NotSupported');
     });
   });
 
   describe("safeTransferFrom", function () {
     it("safeTransferFrom is not supported", async function () {
       await expect(votertoken["safeTransferFrom(address,address,uint256)"](await acc1.getAddress(), await acc2.getAddress(), 1)).to.be
-        .revertedWith('NotSupported()');
+        .revertedWithCustomError(votertoken, 'NotSupported');
     });
   });
 
@@ -47,21 +47,21 @@ describe("VoterToken", function () {
     var bytes = new Uint8Array(1024);
     it("safeTransferFrom is not supported", async function () {
       await expect(votertoken["safeTransferFrom(address,address,uint256,bytes)"](await acc1.getAddress(), await acc2.getAddress(), 1, bytes)).to.be
-        .revertedWith('NotSupported()');
+        .revertedWithCustomError(votertoken, 'NotSupported');
     });
   });
 
   describe("approve", function () {
     it("approve is not supported", async function () {
       await expect(votertoken.approve(await acc1.getAddress(), 1)).to.be
-        .revertedWith('NotSupported()');
+        .revertedWithCustomError(votertoken, 'NotSupported');
     });
   });
 
   describe("setApprovalForAll", function () {
     it("setApprovalForAll is not supported", async function () {
       await expect(votertoken.setApprovalForAll(await acc1.getAddress(), true)).to.be
-        .revertedWith('NotSupported()');
+        .revertedWithCustomError(votertoken, 'NotSupported');
     });
   });
 });
